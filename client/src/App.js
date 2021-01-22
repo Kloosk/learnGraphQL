1b1a1f37ee97f91d01adbbf5f3e00936b52708d8
Lukasz <lukasz.gras.97@gmail.com>
2021-01-22 23:07:54 +0100

    Added client basic configuration

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+function App() {
+  return (
+      <ApolloProvider client={client}>
+        <div>
+          <h2>My first Apollo app 🚀</h2>
+        </div>
+      </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/BookList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { gql } from '@apollo/client';
+
+const BookList = () => {
+    return (
+      <div>
+
+      </div>
+    );
+};
+
+export default BookList;
